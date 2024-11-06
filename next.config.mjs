d2cc5695a8321a2b26f828219d2c831b64e38b22
128lvt <128vanthoai@gmail.com>
2024-11-06 15:40:04 +0700

    config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['206.189.42.99','localhost'], // Thêm 'localhost' vào đây
+        domains: ['https://206.189.42.99:8443','localhost'], // Thêm 'localhost' vào đây
       },
 };
 
