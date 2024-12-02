b1ad39a2d1db6175159fc9e6e12d5a5fba8b3668
128lvt <128vanthoai@gmail.com>
2024-12-02 20:53:40 +0700

    deploy

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -35,8 +35,7 @@ export async function POST(req: Request) {
     name: 'token',
     value: token,
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
+    sameSite: 'lax',
     maxAge: 259200,
   })
 
